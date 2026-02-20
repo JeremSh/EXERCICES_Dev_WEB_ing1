5d013c1f506daeeff1340262636b1eb5d9cd79c7
jshao2 <jeremy.shao@universite-paris-saclay.fr>
2026-02-20 14:59:03 +0100

    première exercice Input

--- cours_3/forwarded-props/src/input.tsx
+++ cours_3/forwarded-props/src/input.tsx
@@ -1,9 +1,11 @@
-
-const Input = () => {
-    // return a <textarea> if a richText prop is true
-    // return an <input> otherwise
-    // forward / set the received props on the returned elements
-
-}
+const Input = ({ richText, ...resteProps }: any) => {
+  //On vérifie s'il y a une propriété richText, si c'est le cas on return un textarea, sinon on retourne un input
+    if (richText) {
+    // On retourne le textArea avec les propsiété autre que richText
+    return <textarea {...resteProps} />;
+  }
+  // On retourne le input avec les propsiété
+  return <input {...resteProps} />;
+};
 
 export default Input;
